daeb541093347e161dcfe163af707d7cd45124b4
cyprienngantsajorel-hue <cyprienngantsajorel@gmail.com>
2026-06-13 00:30:02 +0100

    Create fancy.js

---
+++ utils/fancy.js
@@ -0,0 +1,13 @@
+module.exports = (text) => {
+    return text
+        .replace(/a/gi, "ᴀ")
+        .replace(/b/gi, "ʙ")
+        .replace(/c/gi, "ᴄ")
+        .replace(/d/gi, "ᴅ")
+        .replace(/e/gi, "ᴇ")
+        .replace(/f/gi, "ғ")
+        .replace(/g/gi, "ɢ")
+        .replace(/h/gi, "ʜ")
+        .replace(/i/gi, "ɪ")
+        .replace(/j/gi, "ᴊ")
+}
